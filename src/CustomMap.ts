87318ea420fb7ba0be9a7f2c3e4f38e1b4216325
Auron <98396887+auronvila@users.noreply.github.com>
2023-08-19 13:42:58 +0300

    added marks and updated the types

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,13 +1,33 @@
+interface Markable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
   constructor(divId: string) {
-    new google.maps.Map(document.getElementById(divId) as HTMLElement, {
-      clickableIcons: true,
-      zoom: 4,
-      center: {
-        lat: 0,
-        lng: 0,
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        clickableIcons: true,
+        zoom: 2,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      }
+    );
+  }
+
+  addMarker(mark: Markable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mark.location.lat,
+        lng: mark.location.lng,
       },
     });
   }
